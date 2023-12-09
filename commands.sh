6af93591bf20687f8d880f3fc6b628b8c866d361
Zachary Gittelman <zachgitt@umich.edu>
2023-12-09 18:25:41 -0500

    made commands file

---
+++ commands.sh
@@ -0,0 +1,1 @@
+mongoimport --uri mongodb+srv://vsuortiz:<PASSWORD>@serverlessinstance0.vbq8qth.mongodb.net/explanation-of-benefits --collection anthem-benefits --type JSON --file /Users/zacharygittelman/Documents/2023-12_254_39B0_in-network-rates_1_of_9.json
